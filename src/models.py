40313787e155f32abc4d605bd9e42662bc62580c
AslanSN <alan.staub@hotmail.com>
2022-01-14 15:24:32 +0000

    models.py exercises DONE, diagram updated

--- src/models.py
+++ src/models.py
@@ -8,28 +8,60 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
+class User(Base):
+    __tablename__ = 'User'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nick = Column(String(50), nullable=False, unique=True)
     name = Column(String(250), nullable=False)
+    surname= Column(String(250))
+    email = Column(String(250), nullable=False, unique=True)
+    
+    follower = relationship("Follower")
+    comment = relationship("Comment")
+    post = relationship("Post")
+    like = relationship("Like")
+    chat = relationship("Chat")
+    
+class Post(Base):
+    __tablename__ = 'Post'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey('User.id'))
+    
+    user = relationship(User)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Media(Base):
+    __tablename__ = 'Media'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    type = Column(String(10))
+    url = Column(String)
+    post_id = Column(Integer, ForeignKey('Post.id'))
+    
+class Follower(Base):
+    __tablename__ = 'Follower'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_from_id = Column(Integer, ForeignKey('User.id'))
+    user_to_id = Column(Integer, ForeignKey('User.id'))
 
-    def to_dict(self):
-        return {}
+class Comment(Base):
+    __tablename__ = 'Comment'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    comment_text = Column(String)
+    author_id = Column(Integer, ForeignKey('User.id'))
+    post_id = Column(Integer, ForeignKey('Post.id'))
 
-## Draw from SQLAlchemy base
+class Like(Base):
+    __tablename__ = 'Like'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    postLiked_id = Column(Integer, ForeignKey('Post.id'))
+    likedBy_id = Column(Integer, ForeignKey('User.id'))
+
+class Chat(Base):
+    __tablename__ = 'Chat'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    message = Column(String)
+    from_id = Column(Integer, ForeignKey('User.id'))
+    to_id = Column(Integer, ForeignKey('User.id'))
+    
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
